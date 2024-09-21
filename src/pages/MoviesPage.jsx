3df811e7f2f1f2c7e460bd4b32530a2602c500f5
Miami <alex.miami.petrov@gmail.com>
2024-09-21 10:41:08 +0300

    m

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import MovieList from "../components/MovieList/MovieList";
 
 const MoviesPage = () => {
-  return <div>MoviesPage</div>;
+  const [movies, setMovies] = useState([]);
+  const handleSearch = async (query) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query=${query}`
+    );
+    const data = await response.json();
+    setMovies(data.results)
+  };
+  return (
+    <div>
+      <h1>Пошук фільмів</h1>
+      <input type="text" onChange={(e) => handleSearch(e.target.value)} />
+      <MovieList movies={movies} />
+    </div>;
+  ) 
 };
 
 export default MoviesPage;
