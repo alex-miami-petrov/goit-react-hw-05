01de3251973b602d07f2e470742271afbcb5ab8b
Miami <alex.miami.petrov@gmail.com>
2024-09-21 09:36:05 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, Suspense } from "react";
 import "./App.css";
-import { NavLink } from "./components/Navigation";
+import Navigation from "./components/Navigation/Navigation";
 import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
@@ -9,7 +10,23 @@ const MoviesPage = lazy(() => import("./pages/MoviesPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/cast" element={<MovieDetailsPage />} />
+          <Route
+            path="/movies/:movieId/reviews"
+            element={<MovieDetailsPage />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
