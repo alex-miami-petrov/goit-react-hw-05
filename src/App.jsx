7ef3311cb574ba6e83ae3becd6196fa9809e89da
Miami <alex.miami.petrov@gmail.com>
2024-09-21 14:06:45 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,3 @@
-// import React, { useState, useEffect, useRef, Suspense } from "react";
-// import "./App.css";
-// import Navigation from "./components/Navigation/Navigation";
-// import { lazy } from "react";
-// import { Route, Routes } from "react-router-dom";
-// import MovieCast from "./components/MovieCast/MovieCast";
-// import MovieReviews from "./components/MovieReviews/MovieReviews";
-
-// const HomePage = lazy(() => import("./pages/HomePage"));
-// const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
-// const MoviesPage = lazy(() => import("./pages/MoviesPage"));
-// const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Navigation />
-//       <Suspense fallback={<div>Loading page...</div>}>
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/movies" element={<MoviesPage />} />
-//           <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-//             <Route path="cast" element={<MovieCast />} />
-//             <Route path="reviews" element={<MovieReviews />} />
-//           </Route>
-//           <Route path="*" element={<NotFoundPage />} />
-//         </Routes>
-//       </Suspense>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
