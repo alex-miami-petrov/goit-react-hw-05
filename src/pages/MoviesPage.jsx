fb693dc45192768004f15695a08d00b5529afcc0
Miami <alex.miami.petrov@gmail.com>
2024-09-23 01:08:25 +0300

    m

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -8,7 +8,7 @@ import { fetchMoviesByQuery } from "../Api/Api";
 function MoviesPage() {
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [query, setQuery] = searchParams.get("query") || "";
+  const query = searchParams.get("query") || "";
 
   const handleSubmit = (e) => {
     e.preventDefault();
